48bfbfdff1b7c1ed4fbcc5500f2891206642db2d
Julio Faracco <jcfaracco@gmail.com>
2015-03-10 21:06:13 -0300

    Adding license into missing files.

--- examples/domain-start.lisp
+++ examples/domain-start.lisp
@@ -1,3 +1,24 @@
+;;;-------------------------------------------------------------------------
+;;; Lispvirt - Common LISP bindings for Libvirt.
+;;;
+;;; Copyright (C) 2015 Julio Cesar Faracco
+;;;
+;;; domain-start.lisp is part of Lispvirt.
+;;;
+;;; Lispvirt is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Lispvirt is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Lispvirt.  If not, see <http://www.gnu.org/licenses/>.
+;;;-------------------------------------------------------------------------
+
 (asdf:load-system :lispvirt)
 
 (in-package lispvirt-domain)
