272680f356d68661dd52adfb878839205d56a1d7
Julio Faracco <jcfaracco@gmail.com>
2015-05-01 15:53:16 -0300

    Adding enum structures into lispvirt-storage.

--- lispvirt/src/lispvirt-storage.lisp
+++ lispvirt/src/lispvirt-storage.lisp
@@ -91,6 +91,81 @@
 
 (defcstruct virStorageVolInfo)
 
+(defcenum virStoragePoolState
+	(:VIR_STORAGE_POOL_INACTIVE 0)		;; Not running.
+	(:VIR_STORAGE_POOL_BUILDING 1)		;; Initializing pool, not available.
+	(:VIR_STORAGE_POOL_RUNNING 2)		;; Running normally.
+	(:VIR_STORAGE_POOL_DEGRADED 3)		;; Running degraded.
+	(:VIR_STORAGE_POOL_INACCESSIBLE 4)	;; Running, but not accessible.
+	(:VIR_STORAGE_POOL_STATE_LAST 5))
+
+(defcenum virStoragePoolBuildFlags
+	(:VIR_STORAGE_POOL_BUILD_NEW 0)			;; Regular build from scratch.
+	(:VIR_STORAGE_POOL_BUILD_REPAIR 1)		;; Repair / reinitialize.
+	(:VIR_STORAGE_POOL_BUILD_RESIZE 2)		;; Extend existing pool.
+	(:VIR_STORAGE_POOL_BUILD_NO_OVERWRITE 4)	;; Do not overwrite existing pool.
+	(:VIR_STORAGE_POOL_BUILD_OVERWRITE 8))		;; Overwrite data.
+
+(defcenum virStoragePoolDeleteFlags
+	(:VIR_STORAGE_POOL_DELETE_NORMAL 0)	;; Delete metadata only (fast).
+	(:VIR_STORAGE_POOL_DELETE_ZEROED 1))	;; Clear all data to zeros (slow).
+
+(defcenum virStorageVolType
+	(:VIR_STORAGE_VOL_FILE 0)	;; Regular file based volumes.
+	(:VIR_STORAGE_VOL_BLOCK 1)	;; Block based volumes.
+	(:VIR_STORAGE_VOL_DIR 2)	;; Directory-passthrough based volume.
+	(:VIR_STORAGE_VOL_NETWORK 3)	;; Network volumes like RBD (RADOS Block Device).
+	(:VIR_STORAGE_VOL_NETDIR 4)	;; Network accessible directory that can
+	(:VIR_STORAGE_VOL_LAST 5))
+
+(defcenum virStorageVolDeleteFlags
+	(:VIR_STORAGE_VOL_DELETE_NORMAL 0)	;; Delete metadata only (fast).
+	(:VIR_STORAGE_VOL_DELETE_ZEROED 1))	;; Clear all data to zeros (slow).
+
+(defcenum virStorageVolWipeAlgorithm
+	(:VIR_STORAGE_VOL_WIPE_ALG_ZERO 0)		;; 1-pass, all zeroes.
+	(:VIR_STORAGE_VOL_WIPE_ALG_NNSA 1)		;; 4-pass  NNSA Policy Letter NAP-14.1-C (XVI-8).
+	(:VIR_STORAGE_VOL_WIPE_ALG_DOD 2)		;; 4-pass DoD 5220.22-M section 8-306 procedure.
+	(:VIR_STORAGE_VOL_WIPE_ALG_BSI 3)		;; 9-pass method recommended by the German Center of Security in Information Technologies.
+	(:VIR_STORAGE_VOL_WIPE_ALG_GUTMANN 4)		;; The canonical 35-pass sequence.
+	(:VIR_STORAGE_VOL_WIPE_ALG_SCHNEIER 5)		;; 7-pass method described by Bruce Schneier in "Applied Cryptography" (1996).
+	(:VIR_STORAGE_VOL_WIPE_ALG_PFITZNER7 6)		;; 7-pass random.
+	(:VIR_STORAGE_VOL_WIPE_ALG_PFITZNER33 7)	;; 33-pass random.
+	(:VIR_STORAGE_VOL_WIPE_ALG_RANDOM 8)		;; 1-pass random.
+	(:VIR_STORAGE_VOL_WIPE_ALG_LAST 9))
+
+(defcenum virStorageXMLFlags
+	(:VIR_STORAGE_XML_INACTIVE 1))	;; dump inactive pool/volume information.
+
+(defcenum virConnectListAllStoragePoolsFlags
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_INACTIVE 1)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_ACTIVE 2)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_PERSISTENT 4)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_TRANSIENT 8)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_AUTOSTART 16)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_NO_AUTOSTART 32)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_DIR 64)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_FS 128)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_NETFS 256)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_LOGICAL 512)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_DISK 1024)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_ISCSI 2048)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_SCSI 4096)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_MPATH 8192)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_RBD 16384)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_SHEEPDOG 32768)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_GLUSTER 65536)
+	(:VIR_CONNECT_LIST_STORAGE_POOLS_ZFS 131072))
+
+(defcenum virStorageVolCreateFlags
+	(:VIR_STORAGE_VOL_CREATE_PREALLOC_METADATA 1)
+	(:VIR_STORAGE_VOL_CREATE_REFLINK 2))		 ;; perform a btrfs lightweight copy.
+
+(defcenum virStorageVolResizeFlags
+	(:VIR_STORAGE_VOL_RESIZE_ALLOCATE 1)	;; force allocation of new size.
+	(:VIR_STORAGE_VOL_RESIZE_DELTA    2)	;; size is relative to current.
+	(:VIR_STORAGE_VOL_RESIZE_SHRINK   4))	;; allow decrease in capacity.
+
 
 ;; Pointers mapping to structures.
 (defctype virStoragePoolPtr (:pointer virStoragePool))
