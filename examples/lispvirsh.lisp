92e600b35b751d3e6e32a548b81432ad4c9b3f9e
Julio Faracco <jcfaracco@gmail.com>
2015-11-06 09:44:52 -0200

    examples: fixing license text of lispvirsh.
    Fixing the name file inside license header section.

--- examples/lispvirsh.lisp
+++ examples/lispvirsh.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;; Copyright (C) 2015 Julio Cesar Faracco
 ;;;
-;;; domain-start.lisp is part of Lispvirt.
+;;; lispvirsh.lisp is part of Lispvirt.
 ;;;
 ;;; Lispvirt is free software: you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
